d6f09a22101e584152392aa606ffab1fbf7d9d94
honganh <nguyenhonganh@zen8labs.com>
2023-08-02 20:31:06 +0700

    remove unuse code

--- backend/src/modules/user/controller.ts
+++ backend/src/modules/user/controller.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import passport from "passport"
+import passport from "./passport"
 const userService = require("./service");
 
 const router = express.Router();
@@ -8,33 +8,6 @@ router.get('', (req, res) => {
   res.send("list user")
 });
 
-import Google, { Profile as GoogleProfile } from "passport-google-oauth20";
-
-
-const GoogleStrategy = Google.Strategy
-
-
-passport.use(new GoogleStrategy({
-  clientID: '1079948418371-plt3a2vo5vsbffl4ioaefov6h0thmv4p.apps.googleusercontent.com',
-  clientSecret: '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',
-  callbackURL: `https://rent.car.test/api/v1/user/google/callback`,
-  passReqToCallback: true 
-},
-function(request: Request, accessToken: string, refreshToken: string, profile: GoogleProfile, done: Google.VerifyCallback) {
-  done(null, profile);
-}
-));
-
-passport.serializeUser((user: Express.User, done) => {
-    done(null, user)
-})
-
-passport.deserializeUser((user: Express.User, done) => {
-    done(null, user)
-})
-
-
-  
 
 // router.get('google/callback', 
 //   passport.authenticate('google', { failureRedirect: '/login' }),
