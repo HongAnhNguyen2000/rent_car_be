7d8b3d8683b7d1f21816b2ca755ee5d95a3f5db5
anhtuyet <anhtuyet-zen8>
2023-08-05 15:13:16 +0700

    fix bug

--- backend/src/modules/car/repository.ts
+++ backend/src/modules/car/repository.ts
@@ -1,14 +1,16 @@
 import { EntityRepository, Repository, getRepository } from 'typeorm';
 import { Car } from '../../entities/car';
 import { dataSource } from '../../utils/dataSource';
-import AppError from '../../utils/appError';
+import { CarImageRepository } from '../carImage/repository';
 
 @EntityRepository(Car)
 export class CarRepository{
   private carRepository: Repository<Car> = null;
+  private carImageRepository: CarImageRepository;
   constructor(
   ) {
     this.carRepository = dataSource.getRepository(Car);
+    this.carImageRepository = new CarImageRepository();
   }
 
   async query(q): Promise<Car[]> {
@@ -42,6 +44,36 @@ export class CarRepository{
       return await this.carRepository.save(car);
     } catch (error) {
       throw "Something was wrong";
+    }
+  }
+
+  async getAll(): Promise<Car[]> {
+    try {
+      const result = [];
+      const cars = await this.query({});
+      for (const car of cars) {
+        const img = await this.carImageRepository.getPrimaryByCarId(car.id);
+        const item = { ...car, image: img ? img.link : ""};
+        result.push(item);
+      }      
+      return result;
+    } catch (error) {
+      throw error.message;
+    }
+  }
+
+  async getById(id: string): Promise<Car> {
+    try {
+      const car = await this.findById(id);
+      if (!car) {
+        return null;
+      }
+
+      const carImages = await this.carImageRepository.getByCarId(car.id);
+      const result = { ...car, images: carImages};
+      return result;
+    } catch (error) {
+      throw error.message;
     }
   }
 }
---
+++ backend/src/services/awsService.ts
@@ -0,0 +1,35 @@
+const { S3 } = require("aws-sdk");
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const { Upload } = require("@aws-sdk/lib-storage");
+
+exports.awsPutObject = async (key, body) => {
+    try {
+        const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+        const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+        const region = process.env.AWS_REGION;
+        const bucket = process.env.AWS_BUCKET;
+        const parallelUploads3 = new Upload({
+            client: new S3Client({
+                credentials: {
+                  accessKeyId,
+                  secretAccessKey
+                },
+                region
+            }),
+            params: {
+                ACL: 'public-read',
+                Bucket: bucket,
+                Key: key,
+                Body: body
+            },      
+          tags: [],
+          queueSize: 4,
+          partSize: 1024 * 1024 * 5, // in bytes, at least 5MB
+          leavePartsOnError: false,
+        });
+        await parallelUploads3.done();
+        return `https://${bucket}.s3.${region}.amazonaws.com/${key}`;
+      } catch (e) {
+        throw e;
+      }
+};
